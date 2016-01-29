b0be8f43c31dc3d353cc91917c0385175c7bdcd0
Benedict Roeser <mail@benedictroeser.de>
2016-01-29 11:24:44 +0100

    Added tests for Integer, Max and Min

--- test/src/Validator/IntegerTest.php
+++ test/src/Validator/IntegerTest.php
@@ -30,24 +30,70 @@ class IntegerTest extends \PHPUnit_Framework_TestCase {
 
     /**
      * @covers Wellid\Validator\Integer::validate
-     * @todo   Implement testValidate().
+     * @dataProvider dataProvider
+     * @param mixed $value
+     * @param boolean $expected
      */
-    public function testValidate() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+    public function testValidate($value, $expected) {
+        $result = $this->object->validate($value);
+
+        $this->assertInstanceOf('Wellid\ValidationResult', $result);
+
+        if ($expected) {
+            $this->assertTrue($result->hasPassed());
+            $this->assertFalse($result->isError());
+            $this->assertEmpty($result->getMessage());
+            $this->assertEquals(\Wellid\ValidationResult::ERR_NONE, $result->getCode());
+            $this->assertEquals('passed', (string) $result);
+        } else {
+            $this->assertFalse($result->hasPassed());
+            $this->assertTrue($result->isError());
+            $this->assertNotEmpty($result->getMessage());
+            $this->assertNotEquals(\Wellid\ValidationResult::ERR_NONE, $result->getCode());
+            $this->assertNotEquals('passed', (string) $result);
+        }
+    }
+
+    /**
+     * Dataprovider for testValidate and testValidateBool
+     * 
+     * @return array()
+     */
+    public function dataProvider() {
+        return array(
+            array(null, false),
+            array('TRUE', false),
+            array('abc', false),
+            array(1, true),
+            array(1.0, true),
+            array(1.1, false),
+            array(true, true),
+            array(0.1, false),
+            array(0.999, false),
+            array(434, true),
+            array('3', true),
+            array('3.3', false),
+            array('', false)
         );
+    }
+    
+    /**
+     * 
+     */
+    public function testArrayAndObjectValidation() {
+        $this->assertFalse($this->object->validateBool(array(true)));
+        $x = new \stdClass();
+        $x->y = 'z';
+        $this->assertFalse($this->object->validateBool($x));
     }
 
     /**
      * @covers Wellid\Validator\Integer::validateBool
-     * @todo   Implement testValidateBool().
+     * @dataProvider dataProvider
+     * @param mixed $value
+     * @param boolean $expected
      */
-    public function testValidateBool() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+    public function testValidateBool($value, $expected) {
+        $this->assertEquals($expected, $this->object->validateBool($value));
     }
-
 }
